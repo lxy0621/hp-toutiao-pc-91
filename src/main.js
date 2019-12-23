51b008ea973edbf9599eaf465f9fd93fc7af9e36
lxy <lxy@cfanle.com>
2019-12-23 15:02:27 +0800

    element-ui使用

--- src/main.js
+++ src/main.js
@@ -1,9 +1,16 @@
 import Vue from 'vue'
 import App from './App.vue'
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+// 1.router文件夹默认会加载文件夹下index.js文件
+import router from '@/router'
+
+Vue.use(ElementUI)
 
 Vue.config.productionTip = false
 
 new Vue({
+  router,
   render: h => h(App)
 }).$mount('#app')
 // main.js入口文件
---
+++ src/router/index.js
@@ -0,0 +1,16 @@
+// 配置路由
+import VueRouter from 'vue-router'
+
+import Vue from 'vue'
+
+Vue.use(VueRouter)
+
+// 实例化Vue对象
+const router = new VueRouter({
+  routes: [
+
+  ]
+})
+
+// 导出实例
+export default router
