7b77d46e743a2b6938044d306f2d758a109763b0
lxy <lxy@cfanle.com>
2019-12-23 15:38:47 +0800

    添加图片

--- src/router/index.js
+++ src/router/index.js
@@ -3,12 +3,13 @@ import VueRouter from 'vue-router'
 
 import Vue from 'vue'
 
+import Login from '@/views/login'
 Vue.use(VueRouter)
 
 // 实例化Vue对象
 const router = new VueRouter({
   routes: [
-
+    { path: '/login', component: Login }
   ]
 })
 
---
+++ src/views/login/index.vue
@@ -0,0 +1,37 @@
+<template>
+  <div id="app">
+    <div class="container-login">
+      <el-card class="my-card">
+      </el-card>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style lang="less" scoped>
+.container-login {
+  width: 100%;
+  height:100%;
+  // background-color: #ccc;
+  background: url(../../assets/background-pic.jpg)no-repeat center / cover;
+  position: absolute;
+  top:0;
+  left:0
+}
+.my-card{
+    width: 400px;
+    height: 350px;
+    position: absolute;
+    left: 50%;
+    top: 50%;
+    transform: translate(-50%,-60%);
+    img{
+      display: block;
+      width: 200px;
+      margin: 0 auto;
+    }
+  }
+</style>
